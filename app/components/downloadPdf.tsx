8b7954cbd032e86f977f2216e4f07a9fa4e291a3
Eddy Szydlowiski <30767097+spidercodeur@users.noreply.github.com>
2024-10-24 23:00:56 +0200

    folder files

--- app/components/downloadPdf.tsx
+++ app/components/downloadPdf.tsx
@@ -3,8 +3,9 @@
 const DownloadPdf = () => {
 	return (
 		<a
-			href="/cv-developpeur.pdf"
+			href="/files/cv_developpeur.pdf"
 			target="_blank"
+			rel="noopener noreferrer"
 			className="active:bg-blue-700 bg-primary duration-150 ease-in-out focus:border-blue-700 focus:outline-none focus:shadow-outline-blue hover:bg-purple-600 inline-flex items-center justify-center print:hidden py-2 rounded text-base text-white transition w-full"
 		>
 			Télécharger
