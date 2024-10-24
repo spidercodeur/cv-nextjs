1c9215549ad9556aac95108c761565de3f0c4cc7
Eddy Szydlowiski <30767097+spidercodeur@users.noreply.github.com>
2024-10-24 22:44:49 +0200

    prefetch={false}

--- app/components/downloadPdf.tsx
+++ app/components/downloadPdf.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 const DownloadPdf = () => {
 	return (
 		<Link
-			href="/CV-developpeur.pdf"
+			prefetch={false}
+			href="/cv-developpeur.pdf"
 			target="_blank"
 			className="active:bg-blue-700 bg-primary duration-150 ease-in-out focus:border-blue-700 focus:outline-none focus:shadow-outline-blue hover:bg-purple-600 inline-flex items-center justify-center print:hidden py-2 rounded text-base text-white transition w-full"
 		>
