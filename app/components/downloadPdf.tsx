d07255155c12b1f557cf8a4116a204f19aa766c9
Eddy Szydlowiski <30767097+spidercodeur@users.noreply.github.com>
2024-10-24 22:52:32 +0200

    lien pdf

--- app/components/downloadPdf.tsx
+++ app/components/downloadPdf.tsx
@@ -1,16 +1,14 @@
 "use client";
-import Link from "next/link";
 
 const DownloadPdf = () => {
 	return (
-		<Link
-			prefetch={false}
+		<a
 			href="/cv-developpeur.pdf"
 			target="_blank"
 			className="active:bg-blue-700 bg-primary duration-150 ease-in-out focus:border-blue-700 focus:outline-none focus:shadow-outline-blue hover:bg-purple-600 inline-flex items-center justify-center print:hidden py-2 rounded text-base text-white transition w-full"
 		>
 			Télécharger
-		</Link>
+		</a>
 	);
 };
 export default DownloadPdf;
